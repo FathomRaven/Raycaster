f1ae641d6b5a26fa9a81f6a6adfbf2985aec05b7
Gabe <gabe@nolen.me>
2022-11-07 20:06:34 -0600

    Cleanup commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,37 @@
 /*
-* TODO:
-* Flip maps "x" and "y", to be inline with the vector2,
-* Positions are weird and bugged; Possibly related to above issue. Might be a problem with map loader
-* Something weird going on with non-square textures
-* Transparency doesn't work (?)
-* Modify default texture loading 
-* Aspect ratio deforms the display
-* Sprite Z position does not function correctly
-*   Can not easily play sprite on to floor
-*   Update: Made some progress, but very finicky and annoying
+#################### RAYCASTER ####################
+#         Written by FathomRaven on GitHub        #
+#         Inspired heavily by lodev.com's         #
+#            raycaster tutorial series            #
+#         Art comes from OpenGameArt.com          #
+#             Uses SDL2 for rendering             #
+###################################################
+
+Art sources:
+
+metal_ceiling.png (4-light-grey-metal-plates) by Georges "TRaK" Grondin:
+https://opengameart.org/content/4-light-grey-metal-plates
+metal.png (rusty metal floor) by Sindwiller:
+https://opengameart.org/content/rusty-metal-floor-texture
+bone_wall.png & bone_wall_pillar.png by p0ss:
+https://opengameart.org/content/skull-wall
+bricks.png by alpha_rats:
+https://opengameart.org/content/bricks-tiled-texture-64x64
+stonewall.png by West:
+https://opengameart.org/node/8041
+*/
+
+/*
+TODO:
+Flip maps "x" and "y", to be inline with the vector2,
+Positions are weird and bugged; Possibly related to above issue. Might be a problem with map loader
+Something weird going on with non-square textures
+Add in transparency support
+Modify default texture loading 
+Raycaster is stretched due to changing with and height
+Sprite Z position does not function correctly
+  Can not easily play sprite on to floor
+  Update: Made some progress, but very finicky and annoying
 */
 
 #include <iostream>
